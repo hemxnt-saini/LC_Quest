883600ef6a2efda4d8266f3615c5afc29cd415d4
Hemant Saini <hsainioff@gmail.com>
2020-10-17 20:35:57 +0530

    Rev Array

--- ARRAYS/RevARRAY.cpp
+++ ARRAYS/RevARRAY.cpp
@@ -20,21 +20,49 @@ int main()
 
     // USING STACK
 
+    //     int n;
+    //     cout << "Enter Size of Array" << endl;
+    //     cin >> n;
+    //     int A[n];
+    //     stack<int> S;
+    //     for (int i = 0; i < n; ++i)
+    //     {
+    //         cout << "Enter Elements" << endl;
+    //         cin >> A[i];
+    //         S.push(A[i]);
+    //     }
+
+    //     for (int i = n - 1; i >= 0; i--)
+    //     {
+    //         cout << S.top() << endl;
+    //         S.pop();
+    //     }
+
+    // BEST APPROACH
+
     int n;
     cout << "Enter Size of Array" << endl;
     cin >> n;
     int A[n];
-    stack<int> S;
     for (int i = 0; i < n; ++i)
     {
         cout << "Enter Elements" << endl;
         cin >> A[i];
-        S.push(A[i]);
     }
+    int start, end, temp;
+    start = 0;
+    end = n - 1;
 
-    for (int i = n - 1; i >= 0; i--)
+    while (start < end)
     {
-        cout << S.top() << endl;
-        S.pop();
+        temp = A[start];
+        A[start] = A[end];
+        A[end] = temp;
+        start++;
+        end--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << endl;
     }
 }
