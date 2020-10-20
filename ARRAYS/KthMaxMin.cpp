9ae918b3978783011d9c05e8d2166f1514116c63
Hemant Saini <hsainioff@gmail.com>
2020-10-20 10:42:31 +0530

    KthMinMax

--- ARRAYS/KthMaxMin.cpp
+++ ARRAYS/KthMaxMin.cpp
@@ -5,21 +5,28 @@ using namespace std;
 int main()
 {
 
-    int A[6] = {3, 6, 2, 8, 0, 1};
+    int A[] = {3, 6, 2, 8, 0, 1};
+    int l = sizeof(A) / sizeof(A[0]);
     int n;
     cout << "Enter Kth Element -" << endl;
     cin >> n;
 
-    // int B[6] = {0, 1, 2, 3, 6, 8};
-
-    sort(A, A + 6);
-
+    sort(A, A + l);
     cout << "The " << n << "th smallest element is " << A[n - 1] << endl;
+    // int A[6] = {0, 1, 2, 3, 6, 8};
 
-    for (int i = 5; i >= 0; i--)
+    int start = 0;
+    int end = l - 1;
+
+    while (start < end)
     {
-        cout << A[i];
+        int temp = A[start];
+        A[start] = A[end];
+        A[end] = temp;
+        start++;
+        end--;
     }
+    // int A[6] = {8,6,3,2,1,0};
 
     cout << "The " << n << "th largest element is " << A[n - 1];
 }
