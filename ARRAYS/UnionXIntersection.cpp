897854f411c020259dd1a1d1bfc4877282accc91
Hemant Saini <hsainioff@gmail.com>
2020-10-23 23:37:14 +0530

    UnionXIntersection

--- ARRAYS/UnionXIntersection.cpp
+++ ARRAYS/UnionXIntersection.cpp
@@ -7,19 +7,19 @@ int main()
 
     for (int c = 4; c <= 7; c++)
     {
-        if (a > c)
+        if (a > c) //1>4//6>4
         {
             a = a - 1;
             break;
         }
         else
         {
-            a = a + c;
+            a = a + c; //a=1+4=5
             if (c)
             {
-                a = a + 1;
+                a = a + 1; //a=6
             }
         }
     }
-    cout << a + b;
+    cout << a + b; //6
 }
