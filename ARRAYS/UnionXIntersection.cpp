922c4359dc2fe2da916002d1ef8d100c022c8b90
Hemant Saini <hsainioff@gmail.com>
2020-10-23 23:28:09 +0530

    UnionXIntersection

--- ARRAYS/UnionXIntersection.cpp
+++ ARRAYS/UnionXIntersection.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 int main()
 {
-    int n = 20;
-    if (n)
+    int a = 2, b = 1;
+    for (int c = 2; c <= 4; c++)
     {
-        cout << "hi";
+        a = b + 1; //a=2
+        b = b + a; //b=1+2=3
+
+        if (c + 1 > 2) //2+1>2
+        {
+            continue;
+        }
+        a = a + 1;
     }
-    cout << "helo";
+    cout << a + b;
 }
