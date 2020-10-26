e13fa08ebb0b5c18e769425e255bfba6cedd31cd
Hemant Saini <hsainioff@gmail.com>
2020-10-26 17:25:53 +0530

    zz

--- LINKEDLIST/LinkedListP.c
+++ LINKEDLIST/LinkedListP.c
@@ -1,43 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
+struct node
+{
     int data;
-    struct node* link;
+    struct node *link;
 };
 
-struct node* START=NULL;
+struct node *START = NULL;
 
-void Insert(int n){
-    struct node* nd;
-    struct node* temp;
-    nd=(struct node*)malloc(sizeof(struct node*));
-    if(START==NULL){
-        nd->data=n;
-        nd->link=NULL;
-        START=nd;
-        temp=START;
+void Insert(int n)
+{
+    struct node *nd;
+    struct node *temp;
+    nd = (struct node *)malloc(sizeof(struct node *));
+    if (START == NULL)
+    {
+        nd->data = n;
+        nd->link = NULL;
+        START = nd;
+        temp = START;
     }
-    temp->link=nd;
-    temp=temp->link;
-    nd->data=n;
-    nd->link=NULL;
-}
-
-void Print(){
-struct node* temp=START;
-while(temp != NULL){
-    printf("%d",temp->data);
-    printf(" ");
+    temp->link = nd;
     temp = temp->link;
-}
+    nd->data = n;
+    nd->link = NULL;
 }
 
+void Print()
+{
+    struct node *temp = START;
+    while (temp != NULL)
+    {
+        printf("%d", temp->data);
+        printf(" ");
+        temp = temp->link;
+    }
+}
 
-void main(){
-Insert(3);
-Insert(4);
-Insert(2);
-Insert(5);
-Print();
+void main()
+{
+    Insert(3);
+    Insert(4);
+    Insert(2);
+    Insert(5);
+    Print();
 }
