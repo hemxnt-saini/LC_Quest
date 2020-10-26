a5d1bf54c7149abfb4622349154e2b324433312a
Hemant Saini <hsainioff@gmail.com>
2020-10-26 22:15:36 +0530

    Union

--- ARRAYS/UnionXIntersection.cpp
+++ ARRAYS/UnionXIntersection.cpp
@@ -8,28 +8,42 @@ int main()
     //U={1,2,3,4,5,6,7}
     //I={5}
 
-    int A[5] = {1, 2, 3, 4, 5};
-    int B[3] = {5, 6, 7};
-    int C[8];
+    int n, m, b;
+    cout << "Enter Size of Array 1: ";
+    cin >> n;
+    cout << "Enter Size of Array 2: ";
+    cin >> m;
+    b = n + m;
+    int A[n], B[m], C[b];
 
-    for (int i = 0; i < 5; i++)
+    cout << "Enter Elements of Array 1:" << endl;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        cin >> A[i];
+    }
+    cout << "Enter Elements of Array 2:" << endl;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> B[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
         {
             if (A[i] == B[j])
             {
-                C[j] = B[j];
                 B[j] = 0;
             }
-            else
-            {
-                C[j] = B[j];
-            }
+
+            C[j + n] = B[j];
         }
         C[i] = A[i];
     }
 
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < b; i++)
     {
         cout << C[i] << " ";
     }
