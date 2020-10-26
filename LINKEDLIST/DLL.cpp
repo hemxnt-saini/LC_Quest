41a577250656377bcfcb2b54d1ef516d4dc3c991
Hemant Saini <hsainioff@gmail.com>
2020-10-26 17:24:02 +0530

    zz

--- LINKEDLIST/DLL.cpp
+++ LINKEDLIST/DLL.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
-struct node{
+struct node
+{
     int info;
-    struct node *prev,*next;
+    struct node *prev, *next;
 };
 
 struct node *START;
 
-
-void Print(){
-struct node *p=START;
-while(p != NULL){
-    cout<<p->info<<" ";
-    p=p->next;
-}
-}
-
-void InsertStart(int data){
-struct node *n;
-n=(struct node*)malloc(sizeof(struct node*));
-n->info=data;
-n->prev=NULL;
-if(START==NULL){
-    n->next=NULL;
-    START=n;
-    return;
-}
-n->next=START;
-START=n;
+void Print()
+{
+    struct node *p = START;
+    while (p != NULL)
+    {
+        cout << p->info << " ";
+        p = p->next;
+    }
 }
 
-
-void InsertLast(int data){
-struct node *n,*t,*l;
-t=START;
-n=(struct node*)malloc(sizeof(struct node*));
-n->info=data;
-n->next=NULL;
-if(START==NULL){
-    n->prev=NULL;
-    START=n;
-    return;
+void InsertStart(int data)
+{
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node *));
+    n->info = data;
+    n->prev = NULL;
+    if (START == NULL)
+    {
+        n->next = NULL;
+        START = n;
+        return;
+    }
+    n->next = START;
+    START = n;
 }
 
+void InsertLast(int data)
+{
+    struct node *n, *t, *l;
+    t = START;
+    n = (struct node *)malloc(sizeof(struct node *));
+    n->info = data;
+    n->next = NULL;
+    if (START == NULL)
+    {
+        n->prev = NULL;
+        START = n;
+        return;
+    }
 
-while(t != NULL){
-    l=t;
-    t=t->next;
-}
-n->prev=l;
-l->next=n;
+    while (t != NULL)
+    {
+        l = t;
+        t = t->next;
+    }
+    n->prev = l;
+    l->next = n;
 }
 
 int main()
