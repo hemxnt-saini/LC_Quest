ff0d0acf9ba9c3d4148431c21965948bbb9ed715
Hemant Saini <hsainioff@gmail.com>
2020-10-28 08:53:01 +0530

    Kadane's Algo

--- ARRAYS/KadeneAlgo.cpp
+++ ARRAYS/KadeneAlgo.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){}
+int main()
+{
+    int n;
+    cout << "Enter Size of Array: " << endl;
+    cin >> n;
+    int A[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter Element " << i << ":" << endl;
+        cin >> A[i];
+    }
+
+    int meh = 0, msf = INT8_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        meh = meh + A[i];
+        if (meh < A[i])
+            meh = A[i];
+
+        if (msf < meh)
+            msf = meh;
+    }
+    cout << "The Sum of Largest Contiguous Subarray is : " << msf << endl;
+}
