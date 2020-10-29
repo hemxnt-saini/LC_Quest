7bbf3e4a3092b84c6a3f248f3894032530804fb4
Hemant Saini <hsainioff@gmail.com>
2020-10-29 21:43:22 +0530

    Minimise The Heights

--- ARRAYS/MinHeight.cpp
+++ ARRAYS/MinHeight.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main()
 {
+    int A[6] = {6, 12, 9, 13, 17};
+    int B[6] = {6, 12, 9, 13, 17};
+    stack<int> S;
+    int x;
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = i + 1; j < 6; j++)
+        {
+            x = A[i] - B[j];
+            S.push(x);
+        }
+    }
+
+    while (!S.empty())
+    {
+        cout << S.top() << endl;
+        S.pop();
+    }
 }
