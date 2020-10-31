d2e40226a995b7b9441b51a4300174f4abf602b7
Hemant Saini <hsainioff@gmail.com>
2020-10-31 22:59:52 +0530

    Minimise The Heights

--- ARRAYS/MinHeight.cpp
+++ ARRAYS/MinHeight.cpp
@@ -16,6 +16,11 @@ int main()
         cin >> A[i];
     }
     sort(A, A + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter Element " << i << endl;
+        cin >> A[i];
+    }
     last = A[n - 1] - k;
     first = A[0] + k;
     ans = last - first;
