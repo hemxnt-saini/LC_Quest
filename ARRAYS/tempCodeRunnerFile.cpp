70668a6dd667f12388a1e78d06b443433f8a58be
Hemant Saini <hsainioff@gmail.com>
2020-11-08 17:05:23 +0530

    MinMax

--- ARRAYS/tempCodeRunnerFile.cpp
+++ ARRAYS/tempCodeRunnerFile.cpp
@@ -1,2 +1,14 @@
- // int A[11] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 90};
-    // int end = A[10];
+int start = 0;
+    int end = l - 1;
+
+    while (start < end)
+    {
+        int temp = A[start];
+        A[start] = A[end];
+        A[end] = temp;
+        start++;
+        end--;
+    }
+    // int A[6] = {8,6,3,2,1,0};
+
+    cout << "The " << n << "th largest element is " << A[n - 1];
