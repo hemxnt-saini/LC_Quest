633a475cc9bdb7523322b97f93ab8295b1c89263
Hemant Saini <hsainioff@gmail.com>
2020-11-09 22:29:40 +0530

    MergeIntervals

--- ARRAYS/MergeIntervals.cpp
+++ ARRAYS/MergeIntervals.cpp
@@ -1,6 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    int A[]={{1,4},{5,6},{6,9},{10,11}};
+int main()
+{
+    int A[] = {{1, 4}, {5, 6}, {6, 9}, {10, 11}};
+    for (int i = 0; i < n; i++)
+    {
+        if (A[j] == A[k])
+    }
 }
