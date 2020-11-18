bf15894c7b5a83681cfb249c13a7d824e8f4d142
Hemant Saini <hsainioff@gmail.com>
2020-11-18 22:03:01 +0530

    Selection SORT

--- ARRAYS/tempCodeRunnerFile.cpp
+++ ARRAYS/tempCodeRunnerFile.cpp
@@ -1,14 +1,3 @@
-int start = 0;
-    int end = l - 1;
-
-    while (start < end)
-    {
-        int temp = A[start];
-        A[start] = A[end];
-        A[end] = temp;
-        start++;
-        end--;
-    }
-    // int A[6] = {8,6,3,2,1,0};
-
-    cout << "The " << n << "th largest element is " << A[n - 1];
+    int temp = A[0];
+    A[0] = A[mini];
+    A[mini] = temp;
