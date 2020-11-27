41adf0e684cd41e42f27d9af3d5da7dea40442a3
Hemant Saini <hsainioff@gmail.com>
2020-11-27 13:55:22 +0530

    MergeSort

---
+++ ARRAYS/MergeSort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void mergesort()
+{
+}
+
+int main()
+{
+    int A[10] = {22, 33, 66, 11, 88, 44, 99, 55, 77, 10};
+    mergesort(A, 10);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << A[i] << " ";
+    }
+}
