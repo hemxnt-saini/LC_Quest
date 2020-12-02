a6e5c4d65716f45afad0ef4ee8e5b9d5392eabd5
Hemant Saini <hsainioff@gmail.com>
2020-12-02 19:27:29 +0530

    MS

--- ARRAYS/MergeSort.cpp
+++ ARRAYS/MergeSort.cpp
@@ -8,7 +8,10 @@ void mergesort(int A[], int n)
 int main()
 {
     int A[10] = {22, 33, 66, 11, 88, 44, 99, 55, 77, 10};
-    mergesort(A, 10);
+
+    for (int i = 0; i < 10; i++)
+
+        mergesort(A, 10);
 
     for (int i = 0; i < 10; i++)
     {
