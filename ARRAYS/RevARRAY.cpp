935bbcaf51522fa158ff61eb2a876ee5c52c3f64
Hemant Saini <hsainioff@gmail.com>
2021-01-23 21:34:55 +0530

    Reverse Array

--- ARRAYS/RevARRAY.cpp
+++ ARRAYS/RevARRAY.cpp
@@ -4,20 +4,6 @@ using namespace std;
 
 int main()
 {
-    //     int n;
-    //     cout << "Enter Size of Array" << endl;
-    //     cin >> n;
-    //     int A[n];
-    //     for (int i = 0; i < n; ++i)
-    //     {
-    //         cout << "Enter Elements" << endl;
-    //         cin >> A[i];
-    //     }
-    //     for (int i = n - 1; i >= 0; i--)
-    //     {
-    //         cout << A[i] << endl;
-    //     }
-
     // USING STACK
 
     //     int n;
