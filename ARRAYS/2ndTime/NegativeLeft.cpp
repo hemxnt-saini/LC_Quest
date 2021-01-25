bb1dea66ee4fc579e9fb5f0ded8201053590a238
Hemant Saini <hsainioff@gmail.com>
2021-01-25 22:30:48 +0530

    NegativeLeft

--- ARRAYS/2ndTime/NegativeLeft.cpp
+++ ARRAYS/2ndTime/NegativeLeft.cpp
@@ -4,14 +4,31 @@ using namespace std;
 int main()
 {
 
-    int A[5] = {-1, 4, 3, -2, -5};
+    int A[5] = {-1, -4, -3, 2, -5};
+    int z = 0;
+    int y = 4;
 
     for (int i = 0; i < 5; i++)
     {
-        if (A[i] < 0)
+        if (A[i] >= 0)
         {
-            A[i++] = A[i];
+            cout << "y=" << y << endl;
+            A[y] = A[i];
+            --y;
+            cout << "y=" << y << endl;
         }
-        A[5] = A[i];
+        else
+        {
+            cout << "z=" << z << endl;
+            A[z] = A[i];
+            ++z;
+            cout << "z=" << z << endl;
+        }
+
+        cout << "z=" << z << "y=" << y << endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A[i] << " ";
     }
 }
