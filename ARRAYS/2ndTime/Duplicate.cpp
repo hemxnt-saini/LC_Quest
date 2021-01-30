6e73d00ae614559281ac704bb6bce0f54fe13ff7
Hemant Saini <hsainioff@gmail.com>
2021-01-30 22:19:01 +0530

    Duplicate

--- ARRAYS/2ndTime/Duplicate.cpp
+++ ARRAYS/2ndTime/Duplicate.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int Duplicate(int A[], int n)
+{
+}
+
 int main()
 {
+    int A[] = {3, 2, 4, 2, 5};
+    int n = sizeof(A) / sizeof(A[0]);
+    int ans = Duplicate(A, n);
 }
