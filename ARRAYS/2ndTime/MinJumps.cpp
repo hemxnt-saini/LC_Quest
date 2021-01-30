a6a2da3489f90ae26842aa6df729660193308dda
Hemant Saini <hsainioff@gmail.com>
2021-01-30 22:19:08 +0530

    Min Jumps

--- ARRAYS/2ndTime/MinJumps.cpp
+++ ARRAYS/2ndTime/MinJumps.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int MinJump(int A[], int n)
+{
+}
+
 int main()
 {
+    int A[] = {3, 2, 4, 2, 5};
+    int n = sizeof(A) / sizeof(A[0]);
+    int ans = MinJump(A, n);
 }
