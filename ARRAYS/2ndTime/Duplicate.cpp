c7b0454c23cd84ef379f9da957e1d14e0729be83
Hemant Saini <hsainioff@gmail.com>
2021-01-31 22:04:22 +0530

    Duplicate

--- ARRAYS/2ndTime/Duplicate.cpp
+++ ARRAYS/2ndTime/Duplicate.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 int Duplicate(int A[], int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[i] == A[j])
+            {
+                return A[i];
+            }
+        }
+    }
+    return -1;
 }
 
 int main()
 {
-    int A[] = {3, 2, 4, 2, 5};
+    int A[] = {3, 3, 5, 1, 5};
     int n = sizeof(A) / sizeof(A[0]);
     int ans = Duplicate(A, n);
+    cout << ans;
 }
