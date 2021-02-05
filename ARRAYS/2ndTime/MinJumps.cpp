44e5d02cbd047546f990dde32b590afec9063442
Hemant Saini <hsainioff@gmail.com>
2021-02-05 22:27:02 +0530

    Minimum Jumps

--- ARRAYS/2ndTime/MinJumps.cpp
+++ ARRAYS/2ndTime/MinJumps.cpp
@@ -1,28 +1,70 @@
+// #include <iostream>
+// using namespace std;
+
+// int MinJump(int A[], int n)
+// {
+//     int count = 0;
+//     int sum = 0;
+//     int end = n - 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (sum <= end)
+//         {
+//             sum = sum + A[i];
+//             count = count + 1;
+//         }
+//     }
+
+//     return count - 1;
+// }
+
+// int main()
+// {
+//     int A[] = {1, 4, 3, 2, 6, 7};
+//     int n = sizeof(A) / sizeof(A[0]);
+//     int ans = MinJump(A, n);
+//     cout << ans << endl;
+// }
+
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
-int MinJump(int A[], int n)
-{
-    int count = 0;
-    int sum = 0;
-    int end = n - 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (sum <= end)
-        {
-            sum = sum + A[i];
-            count = count + 1;
-        }
-    }
-
-    return count - 1;
-}
 
 int main()
 {
-    int A[] = {1, 4, 3, 2, 6, 7};
-    int n = sizeof(A) / sizeof(A[0]);
-    int ans = MinJump(A, n);
-    cout << ans << endl;
+    int n, t, count = 1, flag = 0;
+
+    // cout << "Enter Size of Array: " << endl;
+    cin >> n;
+    int A[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        // cout << "Enter Element " << i << endl;
+        cin >> A[i];
+    }
+    sort(A, A + n);
+
+    for (int i = 0; i < n; i++)
+    {
+        // cout << "Enter Element " << i << endl;
+        cin >> A[i];
+    }
+    int end = A[n - 1];
+    for (int i = 1; i < 11; i++)
+    {
+        if (flag != 0)
+        {
+            break;
+        }
+
+        while (A[i] == end)
+        {
+            cout << count << endl;
+            flag = flag + 1;
+            break;
+        }
+        count = count + 1;
+    }
 }
