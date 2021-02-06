bd760030ea9f5235fdcc00c348e209227246a141
Hemant Saini <hsainioff@gmail.com>
2021-02-06 21:27:18 +0530

    Next Permutation

--- ARRAYS/NextPermutation.cpp
+++ ARRAYS/NextPermutation.cpp
@@ -9,6 +9,10 @@ int main()
     sort(arr, arr + 3);
 
     cout << "The 3! possible permutations with 3 elements:\n";
+    do
+    {
+        cout << arr[0] << " " << arr[1] << " " << arr[2] << "\n";
+    } while (next_permutation(arr, arr + 3));
 
     cout << "After loop: " << arr[0] << ' '
          << arr[1] << ' ' << arr[2] << '\n';
